c9c404bcfd1ad1d4326f3677932c5e750a61beb1
Siyam-Bhuiyan <bhuiyansiyam294@gmail.com>
2025-08-23 02:17:36 +0600

    cleaned

--- src/routes/calendar.routes.js
+++
@@ -1,8 +0,0 @@
-// src/routes/calendar.routes.js
-import { Router } from "express";
-import { requireAuth } from "../middleware/auth.js";
-import { myEvents } from "../controllers/calendar.controller.js";
-
-const router = Router();
-router.get("/me", requireAuth, myEvents);
-export default router;
--- src/validators/enrollment.validators.js
+++
@@ -1,6 +0,0 @@
-// src/validators/enrollment.validators.js
-import Joi from "joi";
-
-export const enrollSchema = Joi.object({
-  course_id: Joi.number().integer().positive().required()
-});
