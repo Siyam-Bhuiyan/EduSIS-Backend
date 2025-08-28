650df4a9ed90ccec21841698907b30182be13afe
Siyam-Bhuiyan <bhuiyansiyam294@gmail.com>
2025-08-28 21:53:48 +0600

    initiated

---
+++ edusis/src/main/java/com/edusis/EdusisApplication.java
@@ -0,0 +1,13 @@
+package com.edusis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdusisApplication.class, args);
+    }
+
+}
---
+++ edusis/src/test/java/com/edusis/EdusisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
