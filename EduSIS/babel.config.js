2e26b55d76f830bf641e7ebed52776438a1c9849
Siyam <111516393+Siyam-Bhuiyan@users.noreply.github.com>
2025-09-04 23:25:32 +0600

    changes

---
+++ EduSIS/babel.config.js
@@ -0,0 +1,11 @@
+// babel.config.js
+export default function (api) {
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'],
+      plugins: [
+        'react-native-reanimated/plugin', 
+      ],
+    };
+  };
+  
